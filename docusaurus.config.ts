2ad54e1121c77221c345430a92ca095ab912aedb
algono <algono@inf.upv.es>
2024-08-26 15:03:52 +0200

    Updated edit URL to this repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sabienUPV/pm4h-docs-v3/tree/main/',
         },
         // disable blog
         blog: false,
